c2d0e9beba952d51614d4a71e7f0e401935dedcf
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2022-11-22 18:09:55 -0800

    Adding colors Schema

---
+++ exotic-car-colors/schemas/colorsdata.js
@@ -0,0 +1,73 @@
+export default {
+    name: "Colors",
+    title: "Colors",
+    type: "document",
+    fields: [
+        {
+            name: "brand_type",
+            title: "Brand Type",
+            type: "string",
+        },
+        {
+            name: "color_name",
+            title: "Color Name",
+            type: "string",
+        },
+
+        {
+            name: "short_description",
+            title: "Short Description",
+            type: "string",
+        },
+
+        {
+            name: "long_description",
+            title: "Long Description",
+            type: "text",
+        },
+
+        {
+            name: "hex_text",
+            title: "HEX Text",
+            type: "string",
+        },
+
+        {
+            name: "rgb_text",
+            title: "RGB Text",
+            type: "string",
+        },
+
+        {
+            name: "cmyk_text",
+            title: "CMYK Text",
+            type: "string",
+        },
+
+        {
+            name: "ral_text",
+            title: "RAL Text",
+            type: "string",
+        },
+
+        {
+            name: "related_color_1",
+            title: "Related Color #1",
+            type: "string",
+        },
+
+        {
+            name: "related_color_2",
+            title: "Related Color #2",
+            type: "string",
+        },
+        {
+            name: "related_color_3",
+            title: "Related Color #3",
+            type: "string",
+        }
+    ]
+
+
+    
+}

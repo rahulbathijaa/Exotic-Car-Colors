0e169cb3f3a3439b8d5fac79bba4837c9c054578
Priya BATHIJA <priya.bathija@amadeus.com>
2023-01-04 20:19:35 +0100

    upgraded to react 18 & sanity v3

--- exotic-car-colors/schemas/schema.js
+++ exotic-car-colors/schemas/schema.js
@@ -1,11 +1,6 @@
-import createSchema from "part:@sanity/base/schema-creator";
-import schemaTypes from "all:part:@sanity/base/schema-type";
-
 import colorsdata from './colorsdata' // import the file you just made
 
-export default createSchema({
-  name: "default",
-  types: schemaTypes.concat([
+export default [
 		colorsdata, // add the document type to this array
-  ]),
-});
+  ]
+

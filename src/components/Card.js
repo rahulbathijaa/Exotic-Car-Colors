d7c2e1472e9063bc9d076f0cd6cce81c2edf467e
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2023-03-09 22:50:46 -0500

    implemented lazy load on card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,52 +1,8 @@
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import { getByTitle } from "@testing-library/react";
-
-// export default function Card({ post }) {
-//   console.log(post)
-//   return (
-//     <Link to={`${post.slug && post.slug.current}`} key={post.slug && post.slug.current}>
-
-  
-//       <div
-//         className="max-w-sm rounded overflow-hidden shadow-lg border-black "
-//         style={{ backgroundImage: `url(../img/card-background.png)` }}
-
-//       >
-//         <div className="bg-(../img/card-background.jpg)"></div>
-     
-
-// {post.mainImage && post.mainImage.asset && (
-//           <img
-//             className="w-full content-left rounded-b"
-//             src={post.mainImage.asset.url}
-//             alt={post.short_description}
-//           />
-//         )}
-//         <div className="px-6 py-4 bg-[url('../img/card-background.jpg')]">
-//           <div className="font-bold text-3xl mb-2 text-left">
-//             {post.color_name}
-//           </div>
-
-//           <div className="font-bold text-m mb-2 text-left">
-//             {" "}
-//             {post.short_description}{" "}
-//           </div>
-
-//           <p className="text-gray-700 text-base text-left"> {post.hex_text}</p>
-//           <p className="text-gray-700 text-base text-left"> {post.rgb_text}</p>
-//           <p className="text-gray-700 text-base text-left"> {post.cmyk_text}</p>
-//           <p className="text-gray-700 text-base text-left"> {post.ral_text}</p>
-//         </div>
-//       </div>
-
-//     </Link>
-//   );
-// }
 
 
 import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
+import LazyLoad from 'react-lazyload';
 import colorData from "./colorData";
 import ColorGroupings from "./ColorGroupings";
 
@@ -87,11 +43,13 @@ export default function Card({ post }) {
         style={{ backgroundImage: `url(../img/card-background.png)` }}
       >
         {post.mainImage && post.mainImage.asset && (
-          <img
-            className="w-full content-left rounded-b"
-            src={post.mainImage.asset.url}
-            alt={post.short_description}
-          />
+          <LazyLoad height={200}>
+            <img
+              className="w-full content-left rounded-b"
+              src={post.mainImage.asset.url}
+              alt={post.short_description}
+            />
+          </LazyLoad>
         )}
         <div className="px-6 py-4 bg-[url('../img/card-background.jpg')]">
           <div className="font-bold text-3xl mb-2 text-left">
@@ -111,6 +69,5 @@ export default function Card({ post }) {
     </Link>
   );
 }
-
 
 

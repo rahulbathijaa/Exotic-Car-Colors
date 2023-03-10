651c3b116ae7667932c8bf4dfbacbf8a7c489642
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2023-03-10 00:41:50 -0500

    fixes to make build run

--- exotic-car-colors/sanity.config.js
+++ exotic-car-colors/sanity.config.js
@@ -1,4 +1,3 @@
-// sanity.config.js
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import schemas from "./schemas/schema";
@@ -7,7 +6,7 @@ export default defineConfig({
   title: "Colors",
   projectId: "zlmf9sgs",
   dataset: "production",
-  apiHost: process.env.SANITY_STUDIO_API,
+  apiHost: import.meta.env.VITE_SANITY_STUDIO_API,
   plugins: [deskTool()],
   schema: {
     types: schemas,

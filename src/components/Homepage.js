70645837c48376f4a9c6b1f99f18adfced1a0d81
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2023-06-02 18:31:11 -0700

    testing out updated homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,35 @@
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import { getByTitle } from "@testing-library/react";
+// // import Home from "./components/pages/Home";
+// import Navbar from "./Navbar";
+// import HeroSection from "./HeroSection";
+// import ImageSlider from "./ImageSlider";
+// import SliderData from "./SliderData";
+// import HomepageBrands from "./HomepageBrands";
+// import HomepageColors from "./HomepageColors";
+// import Footer from "./Footer";
+
+// // name
+// // description
+// // etc. ec
+
+// export default function Card({ post }) {
+//   return (
+//     <div>
+//       <Navbar />
+//       <ImageSlider slides={SliderData} />
+//       <HeroSection />
+//       <HomepageBrands />
+//       <HomepageColors />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { getByTitle } from "@testing-library/react";
 // import Home from "./components/pages/Home";
 import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
@@ -10,13 +39,14 @@ import HomepageBrands from "./HomepageBrands";
 import HomepageColors from "./HomepageColors";
 import Footer from "./Footer";
 
-// name
-// description
-// etc. ec
-
 export default function Card({ post }) {
   return (
-    <div>
+    <div
+      className="homepage-container"
+      style={{
+        /* Add your custom styles here */
+      }}
+    >
       <Navbar />
       <ImageSlider slides={SliderData} />
       <HeroSection />

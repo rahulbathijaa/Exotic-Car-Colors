3a0e4e1f6f1f890e9e6f8c2a85bddf0268df9454
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2023-06-02 18:19:01 -0700

    removing the scroll on header

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -57,3 +57,4 @@ export default function AllPosts() {
     </div>
   );
 }
+
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,36 +1,21 @@
-import React, { useState } from "react";
-import { SliderData } from "./SliderData";
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+
+import React from "react";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 import HeroSection from "./HeroSection";
-import image1 from "../img/ferrari-cover-photo.png";
-import image2 from '../img/mclaren-cover-photo.png';
-import image3 from "../img/porsche-cover-photo.png";
+import image1 from "../img/ferrari-cover-photo-1.webp";
 
-const ImageSlider = ({ slides }) => {
-  const [current, setCurrent] = useState(0);
-  const length = slides.length;
-
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
-  if (!Array.isArray(slides) || slides.length <= 0) {
-    return null;
-  }
-
-  const currentImage = SliderData[current].src;
-
+const ImageSlider = () => {
+  const currentImage = image1;
 
   return (
-    // <section className="slider" style={{backgroundImage: `url(${require(`${SliderData[current].image}`).default})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
-      <section className="slider" style={{backgroundImage: `url(${currentImage})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
-      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
-      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
-    </section>
+    <section
+      className="slider"
+      style={{
+        backgroundImage: `url(${currentImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+    />
   );
 };
 

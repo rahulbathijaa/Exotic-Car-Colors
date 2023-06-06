d31df81b88feb711ae2d9a7433306f0a6f14845c
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2023-06-05 19:08:57 -0700

    fixed card links across the site

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,63 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import sanityClient from "../client.js";
-// import Card from "./Card.js";
-// import Footer from "./Footer.js";
-// import { useParams } from "react-router-dom";
-// import Navbar from "./Navbar.js";
-// import HeaderPage from "./HeaderPage.js";
-
-
-// export default function AllPosts() {
-//   const [allPostsData, setAllPosts] = useState(null);
-//   const { brand_type } = useParams();
-
-//   useEffect(() => {
-//     sanityClient
-//       .fetch(
-//         `*[brand_type == "${brand_type}"]{
-//        brand_type,
-//        slug,
-//        color_name,
-//        short_description,
-//        hex_text,
-//        rgb_text,
-//        cmyk_text,
-//        ral_text,
-//        mainImage{
-//         asset->{
-//           _id,
-//           url
-//         }
-//        }
-      
-//         }`
-//       )
-//       .then((data) => {
-//         setAllPosts(data);
-//         console.log("end");
-//       })
-//       .catch(console.error);
-//   }, []);
-
-//   console.log({ allPostsData });
-
-//   return (
-//     <div className="bg-white-100">
-//       <Navbar />
-//       <HeaderPage brand={brand_type} />
-//       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center pt-24">
-//         {allPostsData &&
-//           allPostsData.map((post, index) => {
-//             if (!post.slug || !post.mainImage) return null;
-//             return <Card post={post} />;
-//           })}
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
-
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
@@ -67,6 +7,7 @@ import { useParams } from "react-router-dom";
 import Navbar from "./Navbar.js";
 import HeaderPage from "./HeaderPage.js";
 
+
 export default function AllPosts() {
   const [allPostsData, setAllPosts] = useState(null);
   const { brand_type } = useParams();
@@ -75,20 +16,21 @@ export default function AllPosts() {
     sanityClient
       .fetch(
         `*[brand_type == "${brand_type}"]{
-           brand_type,
-           slug,
-           color_name,
-           short_description,
-           hex_text,
-           rgb_text,
-           cmyk_text,
-           ral_text,
-           mainImage{
-            asset->{
-              _id,
-              url
-            }
-           }
+       brand_type,
+       slug,
+       color_name,
+       short_description,
+       hex_text,
+       rgb_text,
+       cmyk_text,
+       ral_text,
+       mainImage{
+        asset->{
+          _id,
+          url
+        }
+       }
+      
         }`
       )
       .then((data) => {
@@ -98,26 +40,84 @@ export default function AllPosts() {
       .catch(console.error);
   }, []);
 
+  console.log({ allPostsData });
 
   return (
     <div className="bg-white-100">
       <Navbar />
       <HeaderPage brand={brand_type} />
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center pt-24">
-        
-          {allPostsData &&
-  allPostsData.map((post, index) => {
-	  console.log(`/car-color${post.slug.current}`)
-    if (!post.slug || !post.mainImage) return null;
-    return (
-      <Link>
-        <Card post={post} />
-      </Link>
-    );
-  })}
-
+        {allPostsData &&
+          allPostsData.map((post, index) => {
+            if (!post.slug || !post.mainImage) return null;
+            return <Card post={post} />;
+          })}
       </div>
       <Footer />
     </div>
   );
 }
+
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import sanityClient from "../client.js";
+// import Card from "./Card.js";
+// import Footer from "./Footer.js";
+// import { useParams } from "react-router-dom";
+// import Navbar from "./Navbar.js";
+// import HeaderPage from "./HeaderPage.js";
+
+// export default function AllPosts() {
+//   const [allPostsData, setAllPosts] = useState(null);
+//   const { brand_type } = useParams();
+
+//   useEffect(() => {
+//     sanityClient
+//       .fetch(
+//         `*[brand_type == "${brand_type}"]{
+//            brand_type,
+//            slug,
+//            color_name,
+//            short_description,
+//            hex_text,
+//            rgb_text,
+//            cmyk_text,
+//            ral_text,
+//            mainImage{
+//             asset->{
+//               _id,
+//               url
+//             }
+//            }
+//         }`
+//       )
+//       .then((data) => {
+//         setAllPosts(data);
+//         console.log("end");
+//       })
+//       .catch(console.error);
+//   }, []);
+
+
+//   return (
+//     <div className="bg-white-100">
+//       <Navbar />
+//       <HeaderPage brand={brand_type} />
+//       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center pt-24">
+        
+//           {allPostsData &&
+//   allPostsData.map((post, index) => {
+// 	  console.log(`/car-color${post.slug.current}`)
+//     if (!post.slug || !post.mainImage) return null;
+//     return (
+//       <Link>
+//         <Card post={post} />
+//       </Link>
+//     );
+//   })}
+
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }

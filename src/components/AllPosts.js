f21d3c8277ac516aa8aa99b8573352a9282ac7ee
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2023-06-12 21:49:06 -0700

    fixed links to colordata

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,65 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import sanityClient from "../client.js";
-// import Card from "./Card.js";
-// import Footer from "./Footer.js";
-// import { useParams } from "react-router-dom";
-// import Navbar from "./Navbar.js";
-// import HeaderPage from "./HeaderPage.js";
-
-
-// export default function AllPosts() {
-//   const [allPostsData, setAllPosts] = useState(null);
-//   const { brand_type } = useParams();
-
-//   useEffect(() => {
-//     sanityClient
-//       .fetch(
-//         `*[brand_type == "${brand_type}"]{
-//        brand_type,
-//        slug,
-//        color_name,
-//        short_description,
-//        hex_text,
-//        rgb_text,
-//        cmyk_text,
-//        ral_text,
-//        mainImage{
-//         asset->{
-//           _id,
-//           url
-//         }
-//        }
-      
-//         }`
-//       )
-//       .then((data) => {
-//         setAllPosts(data);
-//         console.log("end");
-//       })
-//       .catch(console.error);
-//   }, []);
-
-//   console.log({ allPostsData });
-
-//   return (
-//     <div className="bg-white-100">
-//       <Navbar />
-//       <HeaderPage brand={brand_type} />
-//       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center pt-24">
-//         {allPostsData &&
-//           allPostsData.map((post, index) => {
-            
-//             if (!post.slug || !post.mainImage) return null;
-//             return <Card post={post} />;
-//           })}
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
 import Card from "./Card.js";
@@ -68,13 +7,15 @@ import { useParams } from "react-router-dom";
 import Navbar from "./Navbar.js";
 import HeaderPage from "./HeaderPage.js";
 
-function AllPosts() {
+
+export default function AllPosts() {
   const [allPostsData, setAllPosts] = useState(null);
   const { brand_type } = useParams();
 
-  const fetchData = useCallback(async () => {
-    try {
-      const data = await sanityClient.fetch(`*[brand_type == "${brand_type}"]{
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[brand_type == "${brand_type}"]{
        brand_type,
        slug,
        color_name,
@@ -89,17 +30,15 @@ function AllPosts() {
           url
         }
        }
-    }`);
-      setAllPosts(data);
-      console.log("end");
-    } catch (error) {
-      console.error(error);
-    }
-  }, [brand_type]);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+      
+        }`
+      )
+      .then((data) => {
+        setAllPosts(data);
+        console.log("end");
+      })
+      .catch(console.error);
+  }, []);
 
   console.log({ allPostsData });
 
@@ -110,6 +49,7 @@ function AllPosts() {
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center pt-24">
         {allPostsData &&
           allPostsData.map((post, index) => {
+            
             if (!post.slug || !post.mainImage) return null;
             return <Card post={post} />;
           })}
@@ -118,71 +58,3 @@ function AllPosts() {
     </div>
   );
 }
-
-const MemoizedAllPosts = React.memo(AllPosts);
-export default MemoizedAllPosts;
-
-
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import sanityClient from "../client.js";
-// import Card from "./Card.js";
-// import Footer from "./Footer.js";
-// import { useParams } from "react-router-dom";
-// import Navbar from "./Navbar.js";
-// import HeaderPage from "./HeaderPage.js";
-
-// export default function AllPosts() {
-//   const [allPostsData, setAllPosts] = useState(null);
-//   const { brand_type } = useParams();
-
-//   useEffect(() => {
-//     sanityClient
-//       .fetch(
-//         `*[brand_type == "${brand_type}"]{
-//            brand_type,
-//            slug,
-//            color_name,
-//            short_description,
-//            hex_text,
-//            rgb_text,
-//            cmyk_text,
-//            ral_text,
-//            mainImage{
-//             asset->{
-//               _id,
-//               url
-//             }
-//            }
-//         }`
-//       )
-//       .then((data) => {
-//         setAllPosts(data);
-//         console.log("end");
-//       })
-//       .catch(console.error);
-//   }, []);
-
-
-//   return (
-//     <div className="bg-white-100">
-//       <Navbar />
-//       <HeaderPage brand={brand_type} />
-//       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center pt-24">
-        
-//           {allPostsData &&
-//   allPostsData.map((post, index) => {
-// 	  console.log(`/car-color${post.slug.current}`)
-//     if (!post.slug || !post.mainImage) return null;
-//     return (
-//       <Link>
-//         <Card post={post} />
-//       </Link>
-//     );
-//   })}
-
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,80 @@
-import React, { useEffect, useState, useRef } from "react";
-import { Link } from "react-router-dom";
-import LazyLoad from 'react-lazyload';
-import colorData from "./colorData";
-import ColorGroupings from "./ColorGroupings";
+// import React, { useEffect, useState, useRef } from "react";
+// import { Link } from "react-router-dom";
+// import LazyLoad from 'react-lazyload';
+// import colorData from "./colorData";
+// import ColorGroupings from "./ColorGroupings";
 
-export default function Card({ post }) {
+// export default function Card({ post }) {
+//   const [isVisible, setIsVisible] = useState(false);
+//   const cardRef = useRef(null);
+
+//   useEffect(() => {
+//     const observer = new IntersectionObserver((entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           setIsVisible(true);
+//           observer.unobserve(entry.target);
+//         }
+//       });
+//     });
+//     observer.observe(cardRef.current);
+
+//     // cleanup function for when component unmounts
+//     return () => {
+//       observer.disconnect();
+//     };
+//   }, []);
+
+//   console.log(post)
+
+//   return (
+//     <Link
+//       // to={`/car-color${post.slug.current || post.slug}`}
+//       to={post.slug.current ? `/car-color${post.slug.current}` : post.slug}
+//       key={post.slug && post.slug.current}
+//     >
+
+//       <div
+//         ref={cardRef}
+//         className={`max-w-sm rounded overflow-hidden shadow-lg border-black ${
+//           isVisible ? "animate-fadeInUp" : ""
+//         }`}
+//         style={{ backgroundImage: `url(../img/card-background.png)` }}
+//       >
+//         {post.mainImage && post.mainImage.asset && (
+//           <LazyLoad height={200}>
+//             <img
+//               className="w-full content-left rounded-b"
+//               src={post.mainImage.asset.url}
+//               alt={post.short_description}
+//             />
+//           </LazyLoad>
+//         )}
+//         <div className="px-6 py-4 bg-[url('../img/card-background.jpg')]">
+//           <div className="font-bold text-3xl mb-2 text-left">
+//             {post.color_name}
+//           </div>
+
+//           <div className="font-bold text-m mb-2 text-left">
+//             {post.short_description}
+//           </div>
+
+//           <p className="text-gray-700 text-base text-left">{post.hex_text}</p>
+//           <p className="text-gray-700 text-base text-left">{post.rgb_text}</p>
+//           <p className="text-gray-700 text-base text-left">{post.cmyk_text}</p>
+//           <p className="text-gray-700 text-base text-left">{post.ral_text}</p>
+//         </div>
+//       </div>
+//     </Link>
+//   );
+// }
+
+
+import React, { useEffect, useState, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+import LazyLoad from 'react-lazyload';
+
+export default function Card({ post, hexText }) {
   const [isVisible, setIsVisible] = useState(false);
   const cardRef = useRef(null);
 
@@ -19,27 +89,30 @@ export default function Card({ post }) {
     });
     observer.observe(cardRef.current);
 
-    // cleanup function for when component unmounts
+    // Cleanup function for when the component unmounts
     return () => {
       observer.disconnect();
     };
   }, []);
 
-  console.log(post)
+  // Get the current location using useLocation hook
+  const location = useLocation();
+  const isAllPostsPage = location.pathname === "/all-posts";
 
   return (
     <Link
-      // to={`/car-color${post.slug.current || post.slug}`}
       to={post.slug.current ? `/car-color${post.slug.current}` : post.slug}
       key={post.slug && post.slug.current}
     >
-
       <div
         ref={cardRef}
         className={`max-w-sm rounded overflow-hidden shadow-lg border-black ${
           isVisible ? "animate-fadeInUp" : ""
         }`}
-        style={{ backgroundImage: `url(../img/card-background.png)` }}
+        style={{
+          backgroundImage: isAllPostsPage ? "none" : `url(../img/card-background.png)`,
+          backgroundColor: isAllPostsPage ? hexText : "transparent"
+        }}
       >
         {post.mainImage && post.mainImage.asset && (
           <LazyLoad height={200}>
@@ -68,5 +141,3 @@ export default function Card({ post }) {
     </Link>
   );
 }
-
-
